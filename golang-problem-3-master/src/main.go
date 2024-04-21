ac0cc92fcb8db86c9385ac752c49532d6f8aa250
Amir Fakharzadeh <amirfakharzadeh2002@gmail.com>
2024-04-21 17:56:50 +0330

    Initial files problem2

---
+++ golang-problem-3-master/src/main.go
@@ -0,0 +1,22 @@
+package main
+
+type Bale interface {
+	AddUser(username string, isBot bool) (int, error)
+	AddChat(chatname string, isGroup bool, creator int, admins []int) (int, error)
+	SendMessage(userId, chatId int, text string) (int, error)
+	SendLike(userId, messageId int) error
+	GetNumberOfLikes(messageId int) (int, error)
+	SetChatAdmin(chatId, userId int) error
+	GetLastMessage(chatId int) (string, int, error)
+	GetLastUserMessage(userId int) (string ,int, error)
+}
+
+
+
+type BaleImpl struct {
+	// TODO : Implement Bale
+}
+
+func NewBaleImpl() *BaleImpl {
+	panic("Implement me!")
+}
