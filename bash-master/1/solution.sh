82e73faaf2bbd86235268e25f6620920d5f303b4
Amir Fakharzadeh <amirfakharzadeh2002@gmail.com>
2024-04-29 15:16:55 +0330

    feat: add implementation of Q1 (covering digits using sed commands)

--- bash-master/1/solution.sh
+++ bash-master/1/solution.sh
@@ -1,1 +1,10 @@
-cat $1 | 'YOUR COMMAND GOES HERE'
+# Read the given file 
+numbers=$( cat $1 )
+# Mask first 3 parts of each line using sed command
+for (( i=1; i<=4; i++ ))
+do
+    number=$( echo "$numbers" | awk "NR == $i" )
+    masked_variable=$(echo "$number" | sed 's/[0-9]\+/\****/1; s/[0-9]\+/\****/1; s/[0-9]\+/\****/1')
+    echo "$masked_variable"
+done
+
